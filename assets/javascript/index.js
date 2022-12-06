de3d6cb7d1efcf4b68968a5ceff86712b89ad66f
Edward Schults <edward.schults@gmail.com>
2022-12-06 14:53:46 +0100

    refine events to match GTM nedes

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -5,36 +5,51 @@ const renderBadge = () => {
   badge.innerText = cartLS.list().reduce((prev, curr) => prev + curr.quantity, 0)
 }
 
-const cartItemsListeners = () => {
-  const cartItemQuantityButtons = document.querySelectorAll('.cart-item-buttons')
-  cartItemQuantityButtons.forEach((button) => {
+const listenToAdd = (buttons) => {
+  buttons.forEach((button) => {
     button.addEventListener('click', (event) => {
-      const { id, deltaValue } = event.currentTarget.dataset
-      cartLS.quantity(id, parseInt(deltaValue))
+      const { id, name, price, location } = event.currentTarget.dataset
+      if (cartLS.exists(id)) {
+        cartLS.quantity(id, 1)
+      } else {
+        cartLS.add({ id, name, price })
+      }
       dataLayer.push({
-        event: 'updateQuantity',
-        itemId: id,
-        location: 'cart',
-        delta: deltaValue,
-        cart: cartLS.list(),
-        totalPrice: cartLS.total(),
-        totalQuantity: cartLS.list().reduce((prev, curr) => prev + curr.quantity, 0)
+        event: 'addToCart',
+        item: { id, name, price },
+        location: location
       })
     })
   })
+}
+
+const cartItemsListeners = () => {
+  const addToCartButtons = document.querySelectorAll('.cart .add-to-cart')
+  listenToAdd(addToCartButtons)
 
   const cartItemRemoveButtons = document.querySelectorAll('.cart-item-remove')
   cartItemRemoveButtons.forEach((button) => {
     button.addEventListener('click', (event) => {
-      const { id } = event.currentTarget.dataset
+      const { id, name, price, quantity } = event.currentTarget.dataset
       cartLS.remove(id)
       dataLayer.push({
         event: 'removeCartItem',
-        itemId: id,
+        item: { id, name, price },
+        quantity: quantity,
         location: 'cart',
-        cart: cartLS.list(),
-        totalPrice: cartLS.total(),
-        totalQuantity: cartLS.list().reduce((prev, curr) => prev + curr.quantity, 0)
+      })
+    })
+  })
+
+  const removeFromCartButtons = document.querySelectorAll('.remove-from-cart')
+  removeFromCartButtons.forEach((button) => {
+    button.addEventListener('click', (event) => {
+      const { id, name, price } = event.currentTarget.dataset
+      cartLS.quantity(id, -1)
+      dataLayer.push({
+        event: 'removeOneFromCart',
+        item: { id, name, price },
+        location: 'cart',
       })
     })
   })
@@ -50,14 +65,14 @@ const renderCart = () => {
       <td>#${index + 1}</td>
       <td>${item.name}</td>
       <td>
-        <button type="button" class="btn btn-block btn-sm btn-outline-primary cart-item-buttons" data-id="${item.id}" data-delta-value="-1">-</button>
+        <button type="button" class="btn btn-block btn-sm btn-outline-primary remove-from-cart" data-id="${item.id}" data-name="${item.name}" data-price="${item.price}">-</button>
       </td>
       <td>${item.quantity}</td>
       <td>
-        <button type="button" class="btn btn-block btn-sm btn-outline-primary cart-item-buttons" data-id="${item.id}" data-delta-value="1">+</button>
+        <button type="button" class="btn btn-block btn-sm btn-outline-primary add-to-cart" data-id="${item.id}" data-name="${item.name}" data-price="${item.price}" data-location="cart">+</button>
       </td>
       <td class="text-right">${item.price * item.quantity}€</td>
-      <td class="text-right"><button class="btn btn-outline-danger btn-sm cart-item-remove" data-id="${item.id}">Remove</button></td>
+      <td class="text-right"><button class="btn btn-outline-danger btn-sm cart-item-remove" data-id="${item.id}" data-name="${item.name}" data-price="${item.price}" data-quantity="${item.quantity}">Remove</button></td>
     </tr>`
   }).join('');
 
@@ -71,20 +86,7 @@ renderCart();
 cartLS.onChange(renderCart);
 
 const addToCartButtons = document.querySelectorAll('.add-to-cart')
-addToCartButtons.forEach((button) => {
-  button.addEventListener('click', (event) => {
-    const { id, name, price } = event.currentTarget.dataset
-    cartLS.add({ id, name, price })
-    dataLayer.push({
-      event: 'addToCart',
-      itemId: id,
-      location: 'product',
-      cart: cartLS.list(),
-      totalPrice: cartLS.total(),
-      totalQuantity: cartLS.list().reduce((prev, curr) => prev + curr.quantity, 0)
-    })
-  })
-});
+listenToAdd(addToCartButtons)
 
 const contactForm = document.getElementById('form-contact')
 if (contactForm) {
@@ -116,5 +118,4 @@ cards.forEach((card) => {
   card.addEventListener('mouseleave', (event) => {
     event.currentTarget.classList.remove('shadow-sm')
   })
-
 })
